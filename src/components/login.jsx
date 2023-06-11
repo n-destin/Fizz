24c508fcdd05eca01b2973c6d1b20b46a6ece416
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2023-06-11 01:07:06 -0400

    starting

---
+++ src/components/login.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <p>Login here</p>
+    </div>
+  );
+}
+
+export default Login;
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import CountReducer from './count-reducer'
-//creating the root for all reducers, from which other 
-const rootReducer = combineReducers({
-    count:CountReducer,
-})
-
-export default rootReducer
